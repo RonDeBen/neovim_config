d35d02ee669598b9a0eb0ebe84fec6a2a5d02965
Ron DeBenedetti <ron.debenedetti@praeses.com>
2023-04-28 09:46:22 -0500

    initial commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,18 @@
+-- require('base16').setup({
+--     options = {
+--         theme = "nordfox",
+--         transparent = true,
+--     },
+-- })
+
+
+function ColorMyPencils(color)
+	color = color or "monokai-pro"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
